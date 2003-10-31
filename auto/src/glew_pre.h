2cb8d8ec9cb1c946d66912b2f3f2ec85edb4fd51
ikits <ikits@783a27ee-832a-0410-bc00-9f386506c6dd>
2003-10-31 03:10:25 +0000

    *** empty log message ***
    git-svn-id: https://glew.svn.sourceforge.net/svnroot/glew/trunk/glew@160 783a27ee-832a-0410-bc00-9f386506c6dd

--- auto/src/glew_pre.h
+++ auto/src/glew_pre.h
@@ -119,9 +119,10 @@
 #endif
 /* <ctype.h> */
 #if !defined(__CYGWIN__) && !defined(__MINGW32__) && !defined(_WCHAR_T_DEFINED)
-#ifndef _WCHAR_T_DEFINED
-typedef unsigned short wchar_t;
-#define _WCHAR_T_DEFINED
+#  ifndef _WCHAR_T_DEFINED
+     typedef unsigned short wchar_t;
+#    define _WCHAR_T_DEFINED
+#  endif
 #endif
 
 #ifndef GLAPI
