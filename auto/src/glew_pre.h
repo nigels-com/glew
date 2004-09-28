52f2216b0491ce689632c704fad1728d5eb73062
ikits <ikits@783a27ee-832a-0410-bc00-9f386506c6dd>
2004-09-28 20:27:01 +0000

    added GL_VERSION_2_0
    git-svn-id: https://glew.svn.sourceforge.net/svnroot/glew/trunk/glew@300 783a27ee-832a-0410-bc00-9f386506c6dd

--- auto/src/glew_pre.h
+++ auto/src/glew_pre.h
@@ -1096,13 +1096,6 @@ GLAPI void GLAPIENTRY glViewport (GLint x, GLint y, GLsizei width, GLsizei heigh
 
 #endif /* GL_VERSION_1_1 */
 
-#ifndef GL_VERSION_2_0
-#define GL_VERSION_2_0 1
-
-#define GLEW_VERSION_2_0 GLEW_GET_VAR(__GLEW_VERSION_2_0)
-
-#endif /* GL_VERSION_2_0 */
-
 /* ---------------------------------- GLU ---------------------------------- */
 
 /* this is where we can safely include GLU */
