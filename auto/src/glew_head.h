10ebb5967766bc81b938e534047954ac327e562d
ikits <ikits@783a27ee-832a-0410-bc00-9f386506c6dd>
2006-11-21 04:16:46 +0000

    added GL_EXT_timer_query
    git-svn-id: https://glew.svn.sourceforge.net/svnroot/glew/trunk/glew@449 783a27ee-832a-0410-bc00-9f386506c6dd

--- auto/src/glew_head.h
+++ auto/src/glew_head.h
@@ -165,6 +165,18 @@ typedef float GLclampf;
 typedef double GLdouble;
 typedef double GLclampd;
 typedef void GLvoid;
+#if defined(_MSC_VER) && _MSC_VER < 1400
+#  ifdef _WIN64
+typedef __int64 GLint64EXT;
+typedef unsigned __int64 GLuint64EXT;
+#  else
+typedef _W64 int GLint64EXT;
+typedef _W64 unsigned int GLuint64EXT;
+#  endif
+#else
+typedef signed long long GLint64EXT
+typedef unsigned long long GLuint64EXT
+#endif
 
 #define GL_ACCUM 0x0100
 #define GL_LOAD 0x0101
