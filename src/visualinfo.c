3f63c6224c50a3052c01ddad8a518bd764b8bc66
ikits <ikits@783a27ee-832a-0410-bc00-9f386506c6dd>
2007-12-28 01:47:25 +0000

    Year change
    git-svn-id: https://glew.svn.sourceforge.net/svnroot/glew/trunk/glew@521 783a27ee-832a-0410-bc00-9f386506c6dd

--- src/visualinfo.c
+++ src/visualinfo.c
@@ -3,7 +3,7 @@
 **
 ** Copyright (C) Nate Robins, 1997
 **               Michael Wimmer, 1999
-**               Milan Ikits, 2003-2007
+**               Milan Ikits, 2002-2008
 **
 ** visualinfo is a small utility that displays all available visuals,
 ** aka. pixelformats, in an OpenGL system along with renderer version
