6e29c50eacdadd180429dbef4fea9b3c0ac9e7ec
Nigel Stewart <nigels@users.sourceforge.net>
2012-05-24 22:13:59 -0500

    Filter out GL_FLOAT and GL_UNSIGNED_INT from GL_AMD_performance_monitor extension.

--- auto/bin/update_ext.sh
+++ auto/bin/update_ext.sh
@@ -367,6 +367,12 @@ EOT
     void glProgramUniformMatrix4x3dvEXT (GLuint program, GLint location, GLsizei count, GLboolean transpose, const GLdouble *value)
 EOT
 
+# Filter out GL_UNSIGNED_INT and GL_FLOAT from GL_AMD_performance_monitor
+    grep -v 'GL_UNSIGNED_INT ' $1/GL_AMD_performance_monitor > tmp
+    mv tmp $1/GL_AMD_performance_monitor
+    grep -v 'GL_FLOAT ' $1/GL_AMD_performance_monitor > tmp
+    mv tmp $1/GL_AMD_performance_monitor
+
 # clean up
     rm -f $1/*.bak
 
