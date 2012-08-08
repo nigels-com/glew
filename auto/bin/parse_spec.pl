38a3d857549e7ac31b7edb2a1cfa1ead52f72220
Nigel Stewart <nigels@users.sourceforge.net>
2012-08-07 22:06:41 -0500

    Ignore enums that end in ? - these values are not specified or known.

--- auto/bin/parse_spec.pl
+++ auto/bin/parse_spec.pl
@@ -147,7 +147,7 @@ my %regex = (
     prefix   => qr/^(?:[aw]?gl|glX)/, # gl | agl | wgl | glX
     tprefix  => qr/^(?:[AW]?GL|GLX)_/, # GL_ | AGL_ | WGL_ | GLX_
     section  => compile_regex('^(', join('|', @sections), ')$'), # sections in spec
-    token    => qr/^([A-Z0-9][A-Z0-9_x]*):?\s+((?:0x)?[0-9A-F]+)(.*)$/, # define tokens
+    token    => qr/^([A-Z0-9][A-Z0-9_x]*):?\s+((?:0x)?[0-9A-F]+)([^\?]*)$/, # define tokens
     types    => compile_regex('\b(', join('|', keys %typemap), ')\b'), # var types
     voidtype => compile_regex('\b(', keys %voidtypemap, ')\b '), # void type
 );
