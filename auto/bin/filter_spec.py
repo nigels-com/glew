d80fd5514fe532f8550f14c2fa117a0cab5ece62
Nigel Stewart <nigels@users.sourceforge.net>
2015-08-06 10:33:23 +1000

    Specification filtering refinement - no overview or issues sections

--- auto/bin/filter_spec.py
+++ auto/bin/filter_spec.py
@@ -2,7 +2,7 @@
 
 import re
 
-section = re.compile('^(Name|Name Strings?|Number|Dependencies|New Procedures and Functions|New Tokens|Additions to .*|Changes to .*|Modifications to .*|Add new Section .*)\s*$')
+section = re.compile('^(Name|Name Strings?|Number|Dependencies|Overview|Issues|New Procedures and Functions|New Tokens|Additions to .*|Changes to .*|Modifications to .*|Add new Section .*)\s*$')
 token   = re.compile('^\s+(([A-Z0-9][A-Z0-9_x]*):?\s+((?:0x)?[0-9A-F]+)([^\?]*))?\s*$')
 
 if __name__ == '__main__':
@@ -16,14 +16,19 @@ if __name__ == '__main__':
   for i in args:
       lines = open(i).readlines()
       f = open(i,'w')
+
+      # Keep track of the current section as we iterate over the input
       current = ''
       for j in lines:
+
+          # Detect the start of a new section
           m = section.match(j)
           if m:
               current = m.group(1).strip()
               if current in [ 'Name', 'Name String', 'Name Strings', 'Number', 'Dependencies', 'New Procedures and Functions', 'New Tokens']:
                   print >>f, j,
               continue
+
           if current=='New Tokens':
               if token.match(j):
                   print >>f, j,
