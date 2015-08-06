754e051049291a7be7200190140975ef65837e96
Nigel Stewart <nigels@users.sourceforge.net>
2015-08-06 10:43:12 +1000

    Specification filtering refinement - omit contacts, include contacts and notice

--- auto/bin/filter_spec.py
+++ auto/bin/filter_spec.py
@@ -2,8 +2,9 @@
 
 import re
 
-section = re.compile('^(Name|Name Strings?|Number|Dependencies|Overview|Issues|New Procedures and Functions|New Tokens|Additions to .*|Changes to .*|Modifications to .*|Add new Section .*)\s*$')
+section = re.compile('^(Name|Name Strings?|Contact|Notice|Number|Dependencies|Overview|Issues|IP Status|Status|Version|New Procedures and Functions|New Tokens|Additions to .*|Changes to .*|Modifications to .*|Add new Section .*)\s*$')
 token   = re.compile('^\s+(([A-Z0-9][A-Z0-9_x]*):?\s+((?:0x)?[0-9A-F]+)([^\?]*))?\s*$')
+match   = [ 'Name', 'Name String', 'Contact', 'Notice', 'Name Strings', 'Version', 'Number', 'Dependencies', 'New Procedures and Functions', 'New Tokens']
 
 if __name__ == '__main__':
 
@@ -25,13 +26,13 @@ if __name__ == '__main__':
           m = section.match(j)
           if m:
               current = m.group(1).strip()
-              if current in [ 'Name', 'Name String', 'Name Strings', 'Number', 'Dependencies', 'New Procedures and Functions', 'New Tokens']:
+              if current in match:
                   print >>f, j,
               continue
 
           if current=='New Tokens':
               if token.match(j):
                   print >>f, j,
-          elif current in [ 'Name', 'Name String', 'Name Strings', 'Number', 'Dependencies', 'New Procedures and Functions']:
+          elif current in match:
                   print >>f, j,
 
