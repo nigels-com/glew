f4abb657b36307a3cbb18951a65877dd08b07d5d
Nigel Stewart <nigels@users.sourceforge.net>
2015-12-06 12:58:46 +1000

    GLEW_EGL mode for loading functions via eglGetProcAddress

--- auto/src/glewinfo_head.c
+++ auto/src/glewinfo_head.c
@@ -2,7 +2,9 @@
 #include <stdlib.h>
 #include <string.h>
 #include <GL/glew.h>
-#if defined(GLEW_OSMESA)
+#if defined(GLEW_EGL)
+#include <EGL/egl.h>
+#elif defined(GLEW_OSMESA)
 #define GLAPI extern
 #include <GL/osmesa.h>
 #elif defined(_WIN32)
@@ -25,7 +27,8 @@ static FILE* f;
 
 struct createParams
 {
-#if defined(GLEW_OSMESA)
+#if defined(GLEW_EGL)
+#elif defined(GLEW_OSMESA)
 #elif defined(_WIN32)
   int         pixelformat;
 #elif !defined(__APPLE__) && !defined(__HAIKU__) || defined(GLEW_APPLE_GLX)
