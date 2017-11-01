157911971694ca2bb79b5ba9bb31c38210ec74bb
dimitri <dimi309@users.noreply.github.com>
2017-11-01 05:08:27 +0100

    Removed replace_in_file from imports.

--- build/conan/conanfile.py
+++ build/conan/conanfile.py
@@ -2,7 +2,7 @@ import os
 from conans import ConanFile, CMake
 from conans.tools import os_info, SystemPackageTool, ConanException
 from conans import tools, VisualStudioBuildEnvironment
-from conans.tools import build_sln_command, vcvars_command, replace_in_file, download, unzip
+from conans.tools import build_sln_command, vcvars_command, download, unzip
 
 class GlewConan(ConanFile):
     name = "glew"
