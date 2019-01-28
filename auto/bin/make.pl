103b3d156e16074e9225389ea18520a1ff1ec32f
Nigel Stewart <nigels@nigels.com>
2019-01-28 20:55:10 +1000

    Update copyright information for 2019

--- auto/bin/make.pl
+++ auto/bin/make.pl
@@ -1,4 +1,5 @@
 ##
+## Copyright (C) 2008-2019, Nigel Stewart <nigels[]users sourceforge net>
 ## Copyright (C) 2002-2008, Marcelo E. Magallon <mmagallo[]debian org>
 ## Copyright (C) 2002-2008, Milan Ikits <milan ikits[]ieee org>
 ##
