ec7a0864ab9a75db0d6e543f508609faad668e1d
John Tsiombikas <nuclear@mutantstargoat.com>
2026-08-27 14:51:51 +0300

    Change the only two instances of C99-style comments to C89 comments

--- auto/src/glew_head.c
+++ auto/src/glew_head.c
@@ -95,7 +95,7 @@ void* NSGLGetProcAddress (const GLubyte *name)
   addr = dlsym(image, (const char*)name);
   if( addr ) return addr;
 #ifdef GLEW_APPLE_GLX
-  return dlGetProcAddress( name ); // try next for glx symbols
+  return dlGetProcAddress( name ); /* try next for glx symbols */
 #else
   return NULL;
 #endif
@@ -128,7 +128,7 @@ void* NSGLGetProcAddress (const GLubyte *name)
   free(symbolName);
   if( symbol ) return NSAddressOfSymbol(symbol);
 #ifdef GLEW_APPLE_GLX
-  return dlGetProcAddress( name ); // try next for glx symbols
+  return dlGetProcAddress( name ); /* try next for glx symbols */
 #else
   return NULL;
 #endif
